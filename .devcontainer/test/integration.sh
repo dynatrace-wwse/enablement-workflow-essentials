db23e54c423fd59e75a5030fb7e41c0608ab52f2
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-08-27 20:43:50 +0100

    Integration Tests

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -7,12 +7,3 @@ source $REPO_PATH/.devcontainer/test/test_functions.sh
 
 printInfoSection "Running integration Tests for the Enablement Framework"
 
-assertRunningPod dynatrace operator
-
-assertRunningPod dynatrace activegate
-
-assertRunningPod dynatrace oneagent
-
-assertRunningPod todoapp todoapp
-
-assertRunningApp 30100
